8497de47f7941fa779c33bc82e1427d6364a1baf
WTomoharu <67645623+WTomoharu@users.noreply.github.com>
2021-08-17 14:59:35 +0000

    dbe0557

--- index.js
+++ index.js
@@ -2,4 +2,3 @@
 module.exports = (...params) => {
   console.log("from module:", ...params);
 };
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vaW5kZXguanMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbIm1vZHVsZS5leHBvcnRzID0gKC4uLnBhcmFtcykgPT4ge1xuICBjb25zb2xlLmxvZyhcImZyb20gbW9kdWxlOlwiLCAuLi5wYXJhbXMpXG59Il0sCiAgIm1hcHBpbmdzIjogIjtBQUFBLE9BQU8sVUFBVSxJQUFJLFdBQVc7QUFDOUIsVUFBUSxJQUFJLGdCQUFnQixHQUFHO0FBQUE7IiwKICAibmFtZXMiOiBbXQp9Cg==
