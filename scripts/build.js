02cda0154fb705129e34175abb1d80a1a4319cd2
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-08-17 23:29:58 +0900

    init

---
+++ scripts/build.js
@@ -0,0 +1,30 @@
+const esbuild = require('esbuild')
+const fs = require('fs-extra');
+
+// remove old dist dir
+fs.removeSync("./dist")
+
+// build src
+esbuild.build({
+  entryPoints: ["index.js"],
+  outbase: '.',
+  outdir: './dist',
+
+  platform: "node",
+  target: ["node12"],
+  format: "cjs",
+
+  bundle: true,
+  minify: false,
+  sourcemap: "inline",
+}).then(() => {
+  console.log(`build succeeded`);
+}).catch(() => {
+})
+
+// copy static files
+for (const file of [
+  "package.json",
+]) {
+  fs.copySync(`./${file}`, `./dist/${file}`)
+}
